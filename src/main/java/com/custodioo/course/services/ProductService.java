eb17ad1c5275673152863fcc2d2baf3119c68c44
Augusto Vicente <custodioovicente@outlook.com.br>
2021-11-07 13:36:36 -0300

    Product

---
+++ src/main/java/com/custodioo/course/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.custodioo.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.custodioo.course.entities.Product;
+import com.custodioo.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
