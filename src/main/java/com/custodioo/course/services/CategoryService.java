adf86e81c59d70c157d2432cf781b7784cb6905c
Augusto Vicente <custodioovicente@outlook.com.br>
2021-11-07 13:02:35 -0300

    Category

---
+++ src/main/java/com/custodioo/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.custodioo.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.custodioo.course.entities.Category;
+import com.custodioo.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
